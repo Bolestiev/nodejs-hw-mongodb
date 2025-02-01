8b65ec69ca6a68d59dfddec9b437c842104228f0
Anton Bolestiev <antonbolestiev1997@gmail.com>
2025-02-01 11:24:00 +0200

    add changes in POST and UPDATE

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,17 +1,14 @@
 import createHttpError from 'http-errors';
 
-export const validateBody = (shema) => async (req, res, next) => {
+export const validateBody = (schema) => async (req, res, next) => {
   try {
     await schema.validateAsync(req.body, {
       abortEarly: false,
     });
     next();
   } catch (err) {
-    const error = createHttpError(400, 'Bad request', {
-      errors: err.details.map((e) => ({
-        error: e.message,
-        path: e.path,
-      })),
+    const error = createHttpError(400, 'Bad Request', {
+      errors: err.details,
     });
     next(error);
   }
